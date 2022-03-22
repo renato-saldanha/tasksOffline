507091a10d8a35b8c914d72a1ab13c3e3dfa28cd
desenvolvimento@datainfo.com.br <*ndp2019!>
2022-03-21 20:22:56 -0400

    first commit

---
+++ react-native.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  assets: ['./asstes/fonts']
+}
---
+++ src/screens/TaskList.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class TaskList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>TaskList</Text>
+      </View>
+    )
+  }
+}
